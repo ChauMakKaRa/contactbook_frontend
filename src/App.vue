833d5fce4ce07637128e5605e2b701c156c47b50
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-09-18 14:02:12 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+  // export default {
+
+  // }
+</script>
+
+<template>
+  <h1>hello, Vue.js</h1>
+</template>
+
+<style scoped>
+  .page {
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
